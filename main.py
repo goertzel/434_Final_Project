d53159128089dc4189abf1439f479dd01cdda7d0
goertzel <goertzel@oregonstate.edu>
2018-06-12 08:24:08 -0700

    can call evaluation script

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from perceptron    import Perceptron
 from decision_tree import DecisionTree
 import logistic_regression
 import numpy as np
+import os
 
 def main():
 	print "CS 434 Final"
 	X = get_data(1)
-	tree = DecisionTree(X,6)
-	tree.print_tree()
+	# tree = DecisionTree(X,6)
+	# tree.print_tree()
 	# perceptron = Perceptron(np.matrix(X))
 	# perceptron.predict()
+
+	evaluate('pred', 'Sample_Test_Data/groundtruth')
 	
 	
 # Expects predictions as a matrix of form:
@@ -19,6 +22,12 @@ def write_predictions(predictions):
 	f = open("pred.csv", 'w')
 	map(lambda p: f.write(str(p)+'\n'), [p[0] for p in predictions])
 	f.close()
-	
+
+
+def evaluate(pred='pred', gold='gold'):
+	os.system('python eval_simple.py -p '+pred+'.csv -g '+gold+'.csv')
+
+
 if __name__ == "__main__":
 	main()
+ 
