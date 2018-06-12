1db9fd0a2e35ae2919dd589bada50510c5b76cc5
William Selbie <selbiew@oregonstate.edu>
2018-06-12 15:23:08 -0700

    Changes to main and perceptron

--- main.py
+++ main.py
@@ -10,23 +10,22 @@ def main():
 	X = get_data(1)
 	# tree = DecisionTree(X,6)
 	# tree.print_tree()
-	# perceptron = Perceptron(np.matrix(X))
-	# perceptron.predict()
+	perceptron = Perceptron(np.matrix(X))
+	write_predictions(perceptron.predictions)
+	# print [p[1] for p in perceptron.predictions.tolist() if p[1] > 0]
 
-	evaluate('pred', 'Sample_Test_Data/groundtruth')
+	# evaluate('pred', 'Sample_Test_Data/groundtruth')
 	
 	
 # Expects predictions as a matrix of form:
-# Rows x 1
+# Rows x 2
 def write_predictions(predictions):
 	f = open("pred.csv", 'w')
-	map(lambda p: f.write(str(p)+'\n'), [p[0] for p in predictions])
+	map(lambda p: f.write(str(p[0]) + ',' +	str(p[1]) +'\n'), [(p[0,0], p[0,1]) for p in predictions])
 	f.close()
-
 
 def evaluate(pred='pred', gold='gold'):
 	os.system('python eval_simple.py -p '+pred+'.csv -g '+gold+'.csv')
-
 
 if __name__ == "__main__":
 	main()
--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,8 @@
 import numpy as np
+import random
 
 class Perceptron:
-	def __init__(self, training_data, learning_rate = 0.01, error_threshold = 0.01):
+	def __init__(self, training_data, learning_rate = 0.01, error_threshold = 1.0):
 		self.training_data = training_data
 		self.learning_rate = learning_rate
 		self.error_threshold = error_threshold
@@ -17,13 +18,16 @@ class Perceptron:
 		# D = Cols-1 x 1
 	
 		X = np.asmatrix(self.training_data[:, [i for i in xrange(self.training_data.shape[1]-1)]])
-		Y = np.asmatrix(self.training_data[:, self.training_data.shape[1]-1])
+		Y = np.matrix([[1] if r[0] > 0 else [-1] for r in np.matrix(self.training_data[:, self.training_data.shape[1]-1])])
 		W = np.zeros((X.shape[1], 1))
-		D = np.zeros((X.shape[1], 1))
 		
+		epoch = 0
 		while 1:
+			print epoch
+			epoch += 1
+			D = np.zeros((X.shape[1], 1))
 			for i in xrange(X.shape[0]):
-				prediction = W.T * X[i].T
+				prediction = np.matmul(W.T, X[i].T)
 				if np.matmul(Y[i], prediction) <=0:
 					D -= np.matmul(Y[i], X[i]).transpose()
 			D /= X.shape[0]
@@ -33,4 +37,7 @@ class Perceptron:
 				
 	def predict(self):
 		X = np.asmatrix(self.training_data[:, [i for i in xrange(self.training_data.shape[1]-1)]])
-		return X * self.weights
+		likelihoods = X * self.weights
+		classifications = np.matrix([[1 if likelihoods[i,j] > 0 else 0 for i in range(likelihoods.shape[0])] for j in range(likelihoods.shape[1])]).T
+		
+		return np.hstack((likelihoods, classifications))
