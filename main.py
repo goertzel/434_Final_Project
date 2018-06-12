c692df89ccbdca586f1c0a91924b6b8a48676d96
goertzel <goertzel@oregonstate.edu>
2018-06-11 23:21:08 -0700

    decision tree and percepton working

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from format_data   import get_data
 from perceptron    import Perceptron
 from decision_tree import DecisionTree
 import logistic_regression
+import numpy as np
 
 def main():
 	print "CS 434 Final"
-	X = get_data('General_Population',1)
+	X = get_data(1)
 	tree = DecisionTree(X,6)
-	# tree.print_tree()
-	perceptron = Perceptron(X)
+	tree.print_tree()
+	# perceptron = Perceptron(np.matrix(X))
+	# perceptron.predict()
 	
 	
 # Expects predictions as a matrix of form:
