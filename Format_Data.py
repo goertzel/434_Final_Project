5ed5c7002b3f1fbb5a19703a59861319d7ec6943
goertzel <goertzel@oregonstate.edu>
2018-06-11 19:09:55 -0700

    small modifications

--- Format_Data.py
+++ Format_Data.py
@@ -1,19 +1,12 @@
 import numpy as np
 
-np.set_printoptions(precision=4, suppress=True)
+# np.set_printoptions(precision=4, suppress=True)
 
 
 def read_in_data(dir, subject):
-	data_file = open(dir+'/'+'Subject_'+str(subject)+'.csv')
-	X = np.genfromtxt(data_file, delimiter=',')
-
-	ind_file = open(dir+'/'+'list_'+str(subject)+'.csv')
-	I = np.genfromtxt(ind_file, delimiter=',')
-	I = np.asmatrix(I).T
-
-	X = np.delete(X, 0, 1)	# Remove Timestamp
-	X = np.hstack((I, X)) 	# Prepend Indices 
-	
+	X = np.genfromtxt( open(dir+'/'+'Subject_'+str(subject)+'.csv') , delimiter=',')
+	I = np.asmatrix(np.genfromtxt( open(dir+'/'+'list_'+str(subject)+'.csv') , delimiter=',')).T
+	X = np.hstack( (I, np.delete(X, 0, 1)) ) # Remove Timestamp and Prepend Indices 
 	return X
 
 def join_time_cols(X):
@@ -24,24 +17,18 @@ def join_time_cols(X):
 		where = np.where( np.array(xi.tolist()[0][5:9]) == 1)[0]
 		tmp_X.append(where[0]+1 if len(where) else 0)
 
-	# remove columns 5,6,7,8 
-	X = np.delete(X, np.s_[5:9], axis=1)   
-	X = np.insert(X, 5, tmp_X,  1)
-	
+	# Replace columns 5,6,7,8 with combined version
+	X = np.insert(np.delete(X, np.s_[5:9], axis=1), 5, tmp_X,  1)
 	return X
 
 def create_instances(X):
 	new_X = []
 	i = 0
-
 	while (i < len(X)-7):
 		if ( (X.item(i+6,0) - X.item(i,0)) == 6 ):
 			new_X.append((X[i:i+7][:,1:6]).flatten().tolist()[0] + [X.item(i+7, X.shape[1]-1)])
 			# i = i + 6
 		i += 1
-
-	# print len(new_X)
-	# print new_X[0]
 
 	return np.matrix(new_X)
 
